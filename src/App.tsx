402ac5fc1c0cb0a2378bda075aa7c9f7d2b4c06f
villegas-pug <villegas.pug@gmail.com>
2026-02-26 22:19:08 -0500

    [App.txt] Se hizo cambió el color de fondo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@
 const App = () => {
 
   return (
-    <>
+    <div style={{ backgroundColor: 'blueviolet' }}>
       <h1>¡Jenkins - App!</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis eius laboriosam accusantium ea unde perspiciatis ut, voluptatibus, amet numquam iusto corrupti. Cum error quasi perferendis ratione nisi debitis eligendi natus?</p>
-    </>
+    </div>
   )
 }
 
